9e192a7cc2346102e60444a93965d11d1fc90c0f
Vivienne <viviana.toledo@iteso.mx>
2024-09-28 09:05:20 -0600

    grammar correction

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -4,7 +4,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Title of the page
 st.title("LGBTQIA+ Representation in Television :rainbow:")
 st.markdown("_by Viviana Toledo_")
-st.write("Representation plays a central role in the construction of reality, since their function is to translate concepts into symbols that explain and simplify the social meanings of the world around us.")
+st.write("Representation plays a central role in the construction of reality, since its function is to translate concepts into symbols that explain and simplify the social meanings of the world around us.")
 st.write("As a building unit of the collective consciousness, representation has the power to change the social perception of marginalized groups. Through the symbolic world, we can question and dismantle structures and social concepts, such as gender and sexuality.")
 st.write("In this sense, the analysis of LGBTQIA+ representation allows us to bring the issue to light and reconsider the perspectives that are harbored and have been constructed around the collective. Counting, observing and mapping queer representation is an essential tool for the liberation of gender diversities and the fight for a better future.")
 st.write("This project aims to contribute to the conversation through analysis, statistics and reports of the current situation in cable and streaming TV (data provided by GLAAD) and animated shows (data provided by Bradd Carey & A.N. Wilson).")
